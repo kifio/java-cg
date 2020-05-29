1610f7d2f7e7fbfaad6fa630f3a65ab706b689e7
kifio <kg.of.morphine@gmail.com>
2020-05-29 12:15:39 +0200

    Add 3d cube

--- common/src/main/java/Common.java
+++ common/src/main/java/Common.java
@@ -1,9 +1,12 @@
 import com.jogamp.common.nio.Buffers;
 import com.jogamp.opengl.GL4;
-import com.jogamp.opengl.GLContext;
+import com.jogamp.opengl.math.Matrix4;
+import mathlib3D.Matrix3D;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.FloatBuffer;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -20,6 +23,24 @@ public class Common {
                 GL_COLOR, 0,
                 Buffers.newDirectFloatBuffer(BUFFER_ARRAY)
         );
+    }
+
+    public static Matrix3D perspective(float fovy, float aspect, float n, float f) {
+        float q = 0.1f / (float) Math.tan(Math.toRadians(0.5f * fovy));
+        float A = q / aspect;
+        float B = (n + f) / (n - f);
+        float C = (2.0f * n * f) / (n - f);
+        Matrix3D matrix3D = new Matrix3D();
+
+        matrix3D.setElementAt(0, 0, A);
+        matrix3D.setElementAt(1, 1, q);
+        matrix3D.setElementAt(2, 2, B);
+
+        matrix3D.setElementAt(3, 2, -1f);
+        matrix3D.setElementAt(2, 3, C);
+        matrix3D.setElementAt(3, 3, 0f);
+
+        return matrix3D;
     }
 
     public static String[] readShaderSource(String filename) {
